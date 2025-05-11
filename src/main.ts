1d763c2243f1446a25e655c17e6ef3578c32c602
emirurururu3 <misodenoisii@gmail.com>
2025-05-11 13:31:18 +0900

    commit begin

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ declare global {
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
